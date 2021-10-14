48cc206430adc1739844a91f3b93e417160b19cc
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-14 11:35:44 -0500

    POST /observations route

--- src/app.js
+++ src/app.js
@@ -6,9 +6,19 @@ const notFound = require("./errors/notFound");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
+let nextId = 1;
+app.post("/observations", (req, res) => {
+  const newObservation = req.body.data;
+
+  newObservation.observation_id = nextId++;
+
+  req.status(201).json({
+    data: newObservation,
+  });
+});
 
 app.use(notFound);
 app.use(errorHandler);
