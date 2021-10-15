d6edf8cf1409073235ebc2e063ef383d8ab43ede
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-15 09:53:03 -0500

    final touches

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const observationsRouter = require("./observations/observations.router");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -9,16 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let nextId = 1;
-app.post("/observations", (req, res) => {
-  const newObservation = req.body.data;
-
-  newObservation.observation_id = nextId++;
-
-  req.status(201).json({
-    data: newObservation,
-  });
-});
+app.use("/observations", observationsRouter);
 
 app.use(notFound);
 app.use(errorHandler);
